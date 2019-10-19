31444c816d03fcbbb3e26f4dd53e9b405e3fd6f2
KuwabataK <monorod98@gmail.com>
2019-10-19 16:05:21 +0900

    コードを整理

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,22 @@
-import React, { CSSProperties } from 'react'
-import logo from './logo.svg'
+import React from 'react'
 import './App.css'
 import { useInteractJS } from './hooks'
 
-const position = {
-  position: 'relative' as CSSProperties['position']
-  // width: '100%' ,
-  // height: '100%'
-}
-
 const App: React.FC = () => {
-  const int = useInteractJS({width:200})
+  const interact = useInteractJS()
 
   return (
     <div className="App">
-      <header className="App-header" style={position}>
-        <button onClick={() => int.enable()}>有効化</button>
-        <button onClick={() => int.disable()}>無効化</button>
-        <div style={position}>
-          <img
-            ref={int.interactRef}
-            style={int.interactStyle}
-            src={logo}
-            className="App-logo"
-            alt="logo"
-          />
-        </div>
-      </header>
+      <button onClick={() => interact.enable()}>有効化</button>
+      <button onClick={() => interact.disable()}>無効化</button>
+      <div
+        ref={interact.ref}
+        style={{
+          ...interact.style,
+          border: '2px solid #0489B1',
+          backgroundColor: '#A9D0F5'
+        }}
+      />
     </div>
   )
 }
--- src/hooks.ts
+++ src/hooks.ts
@@ -2,8 +2,8 @@ import { useRef, useEffect, useState, CSSProperties } from 'react'
 import interact from 'interactjs'
 
 type Partial<T> = {
-  [P in keyof T]?: T[P];
-};
+  [P in keyof T]?: T[P]
+}
 
 const initPosition = {
   width: 100,
@@ -18,28 +18,30 @@ const initPosition = {
  * refとstyleに指定することで、そのHTML要素のリサイズと移動が可能になる
  * @param position HTML要素の初期座標と大きさ、指定されない場合はinitPositionで指定された値になる
  */
-export function useInteractJS(position: Partial<typeof initPosition> = initPosition) {
+export function useInteractJS(
+  position: Partial<typeof initPosition> = initPosition
+) {
   const [_position, setPosition] = useState({
     ...initPosition,
     ...position
   })
-  const [isEnable, setEnable] = useState(true)
+  const [isEnabled, setEnable] = useState(true)
 
   const interactRef = useRef(null)
   let { x, y, width, height } = _position
 
   const enable = () => {
-    interact(interactRef.current as any)
-    .draggable({
-      inertia: false
-    })
-    .resizable({
-      // resize from all edges and corners
-      edges: { left: true, right: true, bottom: true, top: true },
-      preserveAspectRatio: false,
-      inertia: false
-    })
-    .on('dragmove', (event) => {
+    interact((interactRef.current as unknown) as HTMLElement)
+      .draggable({
+        inertia: false
+      })
+      .resizable({
+        // resize from all edges and corners
+        edges: { left: true, right: true, bottom: true, top: true },
+        preserveAspectRatio: false,
+        inertia: false
+      })
+      .on('dragmove', event => {
         x += event.dx
         y += event.dy
         setPosition({
@@ -49,44 +51,44 @@ export function useInteractJS(position: Partial<typeof initPosition> = initPosit
           y
         })
       })
-    .on('resizemove', event => {
-      width = event.rect.width
-      height = event.rect.height
-      x += event.deltaRect.left
-      y += event.deltaRect.top
-      setPosition({
-        x,
-        y,
-        width,
-        height
+      .on('resizemove', event => {
+        width = event.rect.width
+        height = event.rect.height
+        x += event.deltaRect.left
+        y += event.deltaRect.top
+        setPosition({
+          x,
+          y,
+          width,
+          height
+        })
       })
-    })
   }
 
   const disable = () => {
-    interact(interactRef.current as any).unset()
+    interact((interactRef.current as unknown) as HTMLElement).unset()
   }
 
   useEffect(() => {
-    if (isEnable) {
+    if (isEnabled) {
       enable()
     } else {
       disable()
     }
     return disable
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isEnable])
+  }, [isEnabled])
 
   return {
-    interactRef,
-    interactStyle: {
+    ref: interactRef,
+    style: {
       transform: `translate3D(${_position.x}px, ${_position.y}px, 0)`,
       width: _position.width + 'px',
       height: _position.height + 'px',
       position: 'absolute' as CSSProperties['position']
     },
     position: _position,
-    isEnable,
+    isEnabled,
     enable: () => setEnable(true),
     disable: () => setEnable(false)
   }
